3c276507c215a04b3969638ad724bbf3eedcd35a
AntonPanda08 <roll2001@ukr.net>
2020-08-13 17:29:11 +0300

    init searchInput

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles.css';
+import refs from './js/refs';
+import debouncedSearch from './js/debounce';
+
+refs.spinner.classList.add('is-hidden');
+
+refs.searchInput.addEventListener('input', event => {
+  refs.spinner.classList.remove('is-hidden');
+  debouncedSearch();
+});
---
+++ src/js/debounce.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import refs from './refs';
+import toastrNotify from './toastr';
+import fetchMovies from './fetchMovies';
+
+const debouncedSearch = _.debounce(e => {
+  const inputValue = refs.searchInput.value;
+  refs.spinner.classList.add('is-hidden');
+  if (inputValue.length === 0) {
+    refs.movieGallery.innerHTML = '';
+    refs.pagination.innerHTML = '';
+    toastrNotify.toastrNoInput();
+  }
+  fetchMovies(inputValue);
+}, 800);
+export default debouncedSearch;
