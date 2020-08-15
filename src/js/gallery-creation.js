9d9ad1400a29bf69b96da823518c2674bd03b4a6
Mikhail <werewolfdevlearn@gmail.com>
2020-08-15 21:54:15 +0300

    finish with fetch

--- src/js/gallery-creation.js
+++ src/js/gallery-creation.js
@@ -1,30 +1,9 @@
-import genres from './jsons/genre.json';
 import galleryFetch from './gallery.js';
 import markupGallery from './markup-gallery.js';
 import refs from './refs.js';
 const gallery1 = galleryFetch.defaultFetchMovies();
-console.log(gallery1);
-gallery1.then(arr => getGenres(arr)).then(arr => injectMarkup(arr));
+gallery1.then(arr => injectMarkup(arr));
 
-function getGenres(arr) {
-  arr.forEach(element => {
-    element.genres = [];
-
-    element.genre_ids.forEach(genre_id => {
-      genres.forEach(genre => {
-        // console.log(genre, genre_id);
-
-        if (genre_id === genre.id) {
-          //   console.log(genre.name);
-          element.genres.push(genre.name);
-          //   console.log(element.genres);
-        }
-      });
-    });
-  });
-  console.log(arr);
-  return arr;
-}
 function injectMarkup(arr) {
   const markup = markupGallery(arr);
   refs.movieGallery.insertAdjacentHTML('beforeend', markup);
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,32 +1,47 @@
+import genres from './jsons/genre.json';
 const apiKey = '7f0dad748ff7b4eb073bc2aebbf95174';
+
 export default {
-  searchQuery: 'a',
+  searchQuery: '',
   page: 1,
   defaultFetchMovies() {
-    // const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=''&page=1`;
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
     return fetch(url)
       .then(res => res.json())
       .then(({ results }) => {
         this.incrementPage();
         return results;
-      });
+      })
+      .then(arr => this.getGenres(arr));
   },
   fetchMovies() {
-    // const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=''&page=1`;
-    //const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=${this.page}&include_adult=false&query=${this.query}`;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${this.searchQuery}&page=1&include_adult=false`;
     return fetch(url)
       .then(res => res.json())
       .then(({ results }) => {
         this.incrementPage();
         return results;
-      });
+      })
+      .then(arr => this.getGenres(arr));
   },
 
-  fetchGenre() {
-    const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
-    return fetch(url).then(res => res.json());
+  // fetchGenre() {
+  //   const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+  //   return fetch(url).then(res => res.json());
+  // },
+  getGenres(arr) {
+    const newArrOfmovies = [...arr];
+    newArrOfmovies.forEach(element => {
+      element.genres = [];
+      element.genre_ids.forEach(genre_id => {
+        genres.forEach(genre => {
+          if (genre_id === genre.id) {
+            element.genres.push(genre.name);
+          }
+        });
+      });
+    });
+    return newArrOfmovies;
   },
 
   resetPage() {
@@ -34,6 +49,11 @@ export default {
   },
   incrementPage() {
     this.page += 1;
+  },
+  decrementPage() {
+    if (this.page > 0) {
+      this.page -= 1;
+    }
   },
   get query() {
     return this.searchQuery;
