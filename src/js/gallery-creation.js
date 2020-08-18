9d0c502f92f8c2788cab119f5de97e0a3157f8f4
Dmytro Osin <mr.oda.ua@gmail.com>
2020-08-19 01:10:51 +0300

    added the eventListeners to imgs of cards at gallery-creation.js

--- src/js/gallery-creation.js
+++ src/js/gallery-creation.js
@@ -2,6 +2,7 @@ import galleryFetch from './gallery.js';
 import markupGallery from './markup-gallery.js';
 import refs from './refs.js';
 const debounce = require('debounce');
+let listOfCards = '';
 
 const defaultGallery = galleryFetch.defaultFetchMovies();
 defaultGallery.then(arr => injectMarkup(arr));
@@ -9,4 +10,14 @@ defaultGallery.then(arr => injectMarkup(arr));
 function injectMarkup(arr) {
   const markup = markupGallery(arr);
   refs.movieGallery.insertAdjacentHTML('beforeend', markup);
+  
+  listOfCards = document.querySelectorAll('.poster-image-box');
+  addEventsToCards(listOfCards);
+  //console.log(listOfCards);
+}
+
+function addEventsToCards(cardsList) {
+  cardsList.forEach(el => el.addEventListener('click', event => {
+    console.log(event);
+  }));
 }
