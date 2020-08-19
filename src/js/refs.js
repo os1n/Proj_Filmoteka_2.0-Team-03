5e733a071c897149c869d25653066df3b11f304a
lesogorovalex <galanthusan@gmail.com>
2020-08-19 19:09:42 +0300

    Navigation refinement

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,10 @@
 const refs = {
   header: document.querySelector('.header'),
+  headerLogo: document.querySelector('.header-logo'),
   searchInput: document.querySelector('.header-search'),
   searchIcon: document.querySelector('.search-icon'),
   movieGallery: document.querySelector('.movie-gallery'),
+  libraryGallery: document.querySelector('.library-gallery'),
   spinner: document.querySelector('.spinner-grow'),
   pagination: document.querySelector('#pagination'),
   watchedBtn: document.querySelector('button[data-action="watched"]'),
